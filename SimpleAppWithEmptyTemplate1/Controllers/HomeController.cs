ae0ba7158b9d6a70b81109ad435aac81fc399aa2
Susie Zhou (CSI Interfusion Inc) <v-yuwzh@microsoft.com>
2020-12-23 23:45:36 -0800

    kkk

---
+++ SimpleAppWithEmptyTemplate1/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleAppWithEmptyTemplate1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["message"] = "Message1 from the controller";
+            return View();
+        }
+    }
+}
